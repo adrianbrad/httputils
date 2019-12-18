aa25eb30833d1084cb8436cc5aedbfbd963dfabe
brad <me@adrianbrad.com>
2019-12-18 10:57:35 +0200

    created close method for the graceful shutdown server

--- server.go
+++ server.go
@@ -4,83 +4,61 @@ import (
 	"context"
 	"net"
 	"net/http"
-	"os"
-	"os/signal"
 	"sync"
-	"syscall"
 	"time"
 )
 
 type GracefulShutdownServer struct {
-	server          *http.Server
-	wg              sync.WaitGroup
-	ctx             context.Context
-	cancelFunc      context.CancelFunc
-	shutdownTimeout time.Duration
+	server            *http.Server
+	wg                sync.WaitGroup
+	ctx               context.Context
+	cancelAllRequests context.CancelFunc
+	shutdownTimeout   time.Duration
 }
 
-func NewGracefulShutdownServer(server *http.Server, shutdownTimeout time.Duration) *GracefulShutdownServer {
-	baseContext, cancelContext := context.WithCancel(context.Background())
-	server.BaseContext = func(listener net.Listener) context.Context {
-		return baseContext
-	}
-
+func NewGracefulShutdownServer(baseContext context.Context, server *http.Server, shutdownTimeout time.Duration) *GracefulShutdownServer {
 	s := &GracefulShutdownServer{
 		server:          server,
-		ctx:             baseContext,
-		cancelFunc:      cancelContext,
 		shutdownTimeout: shutdownTimeout,
 	}
 
-	middleware := func(next http.Handler) http.Handler {
+	if baseContext == nil {
+		baseContext = context.Background()
+	}
+
+	s.ctx, s.cancelAllRequests = context.WithCancel(baseContext)
+	s.server.BaseContext = func(listener net.Listener) context.Context {
+		return s.ctx
+	}
+
+	middlewareWaitGroup := func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			s.wg.Add(1)
 			next.ServeHTTP(w, r)
 			s.wg.Done()
 		})
 	}
-	server.Handler = middleware(server.Handler)
+	server.Handler = middlewareWaitGroup(server.Handler)
 
 	return s
 }
 
 func (s *GracefulShutdownServer) Serve(listener net.Listener) error {
-	done := make(chan error)
-	go s.gracefulShutdown(done)
-
-	serveErr := s.server.Serve(listener)
-
-	if err := <-done; err != nil && (serveErr == http.ErrServerClosed || serveErr == nil) {
-		return err
-	}
-	return serveErr
+	return s.server.Serve(listener)
 }
 
 func (s *GracefulShutdownServer) ListenAndServe() error {
-	done := make(chan error)
-	go s.gracefulShutdown(done)
-
-	serveErr := s.server.ListenAndServe()
-
-	if err := <-done; err != nil && (serveErr == http.ErrServerClosed || serveErr == nil) {
-		return err
-	}
-	return serveErr
+	return s.server.ListenAndServe()
 }
 
-func (s *GracefulShutdownServer) gracefulShutdown(done chan error) {
-	stop := make(chan os.Signal)
-	signal.Notify(stop, syscall.SIGTERM)
-	signal.Notify(stop, syscall.SIGINT)
-	<-stop
-	close(stop)
-	s.cancelFunc()
-
+func (s *GracefulShutdownServer) Close() error {
+	s.cancelAllRequests()
 	ctx, cancel := context.WithTimeout(context.Background(), s.shutdownTimeout)
 	defer cancel()
-
 	err := s.server.Shutdown(ctx)
+	if err != nil {
+		return err
+	}
 	s.wg.Wait()
-	done <- err
-	close(done)
+	return nil
 }
--- server_test.go
+++ server_test.go
@@ -1,10 +1,12 @@
 package httputils
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/require"
 	"net"
 	"net/http"
 	"os"
+	"os/signal"
 	"strconv"
 	"sync"
 	"syscall"
@@ -17,27 +19,48 @@ func TestServer(t *testing.T) {
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		wg.Add(1)
 		defer wg.Done()
+
+		fmt.Println("request handler: received request, waiting for shutdown")
 		<-r.Context().Done()
+		fmt.Println("request handler: received shutdown, done")
 	})
-	s := NewGracefulShutdownServer(&http.Server{Handler: handler}, 1*time.Second)
+
+	s := NewGracefulShutdownServer(nil, &http.Server{Handler: handler}, 1*time.Second)
 	listener, err := net.Listen("tcp", ":0")
 	require.NoError(t, err)
 
 	go func() {
 		wg.Add(1)
 		defer wg.Done()
+
+		fmt.Println("serve func: starting serve")
 		require.EqualError(t, s.Serve(listener), http.ErrServerClosed.Error())
+		fmt.Println("serve func: done")
 	}()
 
 	go func() {
+		fmt.Println("send request func: sending")
 		_, err = http.DefaultClient.Get("http://:" + strconv.Itoa(listener.Addr().(*net.TCPAddr).Port))
 		require.NoError(t, err)
+		fmt.Println("send request func: done")
+	}()
+
+	go func() {
+		stop := make(chan os.Signal)
+		signal.Notify(stop, syscall.SIGTERM)
+		signal.Notify(stop, syscall.SIGINT)
+		<-stop
+		close(stop)
+		fmt.Println("shutdown signal func: received signal to shutdown")
+		require.NoError(t, s.Close())
+		fmt.Println("shutdown signal func: done")
 	}()
 
 	time.Sleep(2 * time.Second)
 	p, err := os.FindProcess(os.Getpid())
 	require.NoError(t, err)
 	require.NoError(t, p.Signal(syscall.SIGINT))
-
+	fmt.Println("main func: sent int signal")
 	wg.Wait()
+	fmt.Println("main func: done")
 }
